ba645c763d03a300ef5abb47d56c4b09dee397f6
WaseemWani <42315122+WaseemWani@users.noreply.github.com>
2019-02-22 20:55:58 +0530

    Add files via upload

---
+++ iOSAssignments/Assignment1/LoginApp/LoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Waseem Wani on 19/02/19.
+//  Copyright © 2019 TTN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
