0ce18b48d3188241599f49eb47082275919312d0
WaseemWani <waniwaseemjmi@gmail.com>
2019-03-19 11:04:38 +0530

    assignment 11

---
+++ iOSAssignments/Assignment11/Assignment10/Assignment10/AutoLayoutProgramticallyViewController.swift
@@ -0,0 +1,42 @@
+//
+//  AutoLayoutProgramticallyViewController.swift
+//  Assignment10
+//
+//  Created by Waseem Wani on 16/03/19.
+//  Copyright © 2019 TTN. All rights reserved.
+//
+
+import UIKit
+
+class AutoLayoutProgramticallyViewController: UIViewController {
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var imageInGreen: UIImageView!
+    @IBOutlet weak var labelInGreen: UILabel!
+    
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var imageInYellow: UIImageView!
+    @IBOutlet weak var labelInYellow: UILabel!
+    
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var imageInRed: UIImageView!
+    @IBOutlet weak var labelInRed: UILabel!
+    
+    @IBOutlet weak var blueView: UIView!
+    @IBOutlet weak var imageInBlue: UIImageView!
+    @IBOutlet weak var labelInBlue: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //autoLayout()
+        
+//       let margins = view.layoutMarginsGuide
+//       greenImageView.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+//
+        // Do any additional setup after loading the view.
+        
+    }
+
+}
