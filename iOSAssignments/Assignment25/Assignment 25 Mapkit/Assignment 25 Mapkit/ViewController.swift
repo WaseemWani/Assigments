0ec0126a2d241f47c347fe01489f2bf0067ed605
WaseemWani <waniwaseemjmi@gmail.com>
2019-04-10 17:21:05 +0530

    added assignment 25 localaizatio, mapkit

--- iOSAssignments/Assignment25/Assignment 25 Mapkit/Assignment 25 Mapkit/ViewController.swift
+++ iOSAssignments/Assignment25/Assignment 25 Mapkit/Assignment 25 Mapkit/ViewController.swift
@@ -32,8 +32,16 @@ class ViewController: UIViewController, MKMapViewDelegate {
         
            annotationPoint4.coordinate = CLLocationCoordinate2D(latitude: 23.0225 , longitude:  72.5714)
            mapView.addAnnotation(annotationPoint4)
+        
+        let location = CLLocationCoordinate2D( latitude: 28.7041, longitude: 77.1025)
+        let location2 = CLLocationCoordinate2D(latitude: 22.5726, longitude: 88.3639)
+//        let cirularOverlay = MKCircle(center: location, radius: 10)
+        
+        
+        addCircularOverlay(location)
+        addCircularOverlay(location2)
          // var linearray = [28.7041, 77.1025, 22.5726,  88.3639]
-        //let location = CLLocationCoordinate2D( latitude: 28.7041, longitude: 77.1025)
+        
         //let location2  = CLLocationCoordinate2D( latitude: 17.3850,   longitude: 78.4867)
         //let region = MKCoordinateRegion(//(location, location2)
         //MKCoordinateRegion(center: location, latitudinalMeters: 5000.0, longitudinalMeters: 7000.0)
@@ -42,6 +50,22 @@ class ViewController: UIViewController, MKMapViewDelegate {
         //
     
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if  overlay is MKCircle {
+            let circle = MKCircleRenderer.init(overlay: overlay)
+            circle.strokeColor = UIColor.blue
+            circle.lineWidth = 4.0
+            return circle
+        }
+        return MKCircleRenderer()
+    }
+    
+    func addCircularOverlay(_ location: CLLocationCoordinate2D) {
+        //let location = CLLocationCoordinate2D( latitude: 28.7041, longitude: 77.1025)
+        let cirularOverlay = MKCircle(center: location, radius: 100)
+        mapView.addOverlay(cirularOverlay)
     }
 //    annotation.coordinate = (latitude: point["latitude"] as! Double, longitude: point["longitude"] as! Double)
 //    mapView.addAnnotation(annotation)
