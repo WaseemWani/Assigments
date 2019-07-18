c9c068e8bcfb0946bde63832d713b291119fd358
Waseem Wani <waseemwani@Waseems-MacBook-Pro.local>
2019-07-18 12:29:27 +0530

    added Employees World Project

---
+++ Employees World/View/AppConstants.swift
@@ -0,0 +1,13 @@
+//
+//  AppConstants.swift
+//  Employees World
+//
+//  Created by Waseem Wani on 30/05/19.
+//  Copyright © 2019 TTN. All rights reserved.
+//
+
+import Foundation
+
+struct AppConstants {
+    static let empErrorSubviewTag = 100000
+}
